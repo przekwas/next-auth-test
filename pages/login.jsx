5d5a92b6baf406309c7fb62e2b1dab62b97a6d37
Luke Przkwas <luke@covalence.io>
2022-04-20 22:56:03 -0500

    adding cleaner code

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,12 +1,17 @@
 import { signIn, useSession } from 'next-auth/react';
 import Nav from '../components/Nav';
-import Router from 'next/router'
+import Router from 'next/router';
 
 export default function Login() {
-	const { data: session } = useSession();
+	const { data: session, status } = useSession();
 
-	if (session && session.user.email) {
-		Router.push('/')
+	// logs 'unauthenticated' at firt
+	// then 'loading' then 'authenticated' after clicking
+	// then re-routes
+	console.log(status);
+
+	if (status === 'authenticated') {
+		Router.push('/');
 	}
 
 	return (
@@ -16,7 +21,10 @@ export default function Login() {
 				<section className="row justify-content-center">
 					<div className="col-md-6">
 						<form className="px-3 py-5 border rounded shadow form-group d-flex justify-content-center align-items-center">
-							<button type="button" onClick={() => signIn('github')} className="btn btn-primary">
+							<button
+								type="button"
+								onClick={() => signIn('github')}
+								className="btn btn-primary">
 								<span>Login with Github</span> <i className="bi bi-github"></i>
 							</button>
 						</form>
